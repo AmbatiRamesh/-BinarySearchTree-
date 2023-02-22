3b9b48b8ba04517db65c85f2d6b251e3583e7136
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-22 15:18:14 +0530

    Added: Check if all are added with using size method in BST

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -16,8 +16,13 @@ public class BinarySearchTree {
         } else if (newnode.compareTo(root) < 0) {
             root.LeftNode = insertElements(root.LeftNode, Data);
         }
-        this.root=root;
+        this.root = root;
         return root;
+    }
+    public static int Size(Node root) {
+        if (root == null)
+            return 0;
+        return Size(root.LeftNode) + 1 + Size(root.RightNode);
     }
     public void Inorder(Node node) {
         if (node == null) {
@@ -29,10 +34,22 @@ public class BinarySearchTree {
     }
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
-        Node root = binarySearchTree.insertElements(null, 56);
+        Node root = binarySearchTree.insertElements(null,56);
         Node secondnode = binarySearchTree.insertElements(root, 30);
-        Node thirdnode = binarySearchTree.insertElements(secondnode, 70);
+        Node thirdnode = binarySearchTree.insertElements(root, 70);
+        Node node4th = binarySearchTree.insertElements(root, 22);
+        Node node5th = binarySearchTree.insertElements(root, 40);
+        Node node6th = binarySearchTree.insertElements(root, 11);
+        Node node7t = binarySearchTree.insertElements(root, 3);
+        Node node8th = binarySearchTree.insertElements(root, 16);
+        Node node9th = binarySearchTree.insertElements(root, 60);
+        Node node10th = binarySearchTree.insertElements(root, 95);
+        Node node11th = binarySearchTree.insertElements(root, 65);
+        Node node12th = binarySearchTree.insertElements(root, 63);
+        Node node13th = binarySearchTree.insertElements(root, 67);
         binarySearchTree.PrintOrder();
+        binarySearchTree.Size(root);
+        System.out.println("Tree size is = " + Size(root));
     }
 }
 
